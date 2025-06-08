fb93df20d2b019dae21d28ab189dcb0d0f844c9e
KevinFrutos <vinke.14297@gmail.com>
2025-06-08 16:11:48 +0200

    ADD-AUDIT: Add audit to models

--- src/infrastructure/db/note/NoteModel.ts
+++ src/infrastructure/db/note/NoteModel.ts
@@ -1,9 +1,13 @@
 import { Schema, model } from 'mongoose';
 
-const noteSchema = new Schema({
+const noteSchema = new Schema(
+    {
     text: { type: String, required: true },
     isDone: { type: Boolean, default: false },
     userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
-});
+    },
+    {
+        timestamps: { createdAt: true, updatedAt: true },
+    });
 
 export default model('Note', noteSchema);
